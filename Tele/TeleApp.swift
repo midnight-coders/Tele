c05eaa913973b957f82f7727d7ff6ef29dd8c085
Chris Hand <cj.hand@gmail.com>
2022-10-02 21:34:26 -0700

    Initial Commit

---
+++ Tele/TeleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeleApp.swift
+//  Tele
+//
+//  Created by Chris Hand on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TeleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
